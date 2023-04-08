399e5e296232225370f6c14ddd1049c58f6abb4b
erivan123erivan <96080412+erivan123erivan@users.noreply.github.com>
2023-04-08 13:59:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,8 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const server = require("http").createServer(app);
-const router = express.Router();
 env.config();
 
-app.use("/uploads", express.static("uploads"));
-
-const apiRoutes = require("./routes/api");
-app.use("/api", apiRoutes);
 
 const io = require("socket.io")(server, {
   cors: {
@@ -18,14 +13,12 @@ const io = require("socket.io")(server, {
   },
 });
 
-router.get("/", (req, res) => {
-  res.send("Welcome to the API!");
-});
-
 const authRoutes = require("./routes/auth");
 const socketRouter = require("./routes/socketRouter")(io);
 const messageRoutes = require("./routes/message");
 const uploadRoutes = require("./routes/uploads")
+
+
 
 mongoose
   .connect(
@@ -39,24 +32,34 @@ mongoose
     console.log("Database connected");
   });
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
-});
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static("uploads"));
-app.use("/api", authRoutes);
-app.use("/api", socketRouter);
-app.use("/api", messageRoutes);
-app.use("/api", uploadRoutes);
+  app.get('/', (req, res) => {
+     
+  });
 
-io.on("connection", (socket) => {
-  console.log(`ID: ${socket.id}`);
-});
+  
+  app.get('/api', (req, res) => {
+     
+  });
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${ process.env.PORT }`);
-});
+  
 
-module.exports = router;
+
+  
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.static("uploads"));
+  app.use("/api", authRoutes);
+  app.use("/api", socketRouter);
+  app.use("/api", messageRoutes);
+  app.use("/api", uploadRoutes);
+  
+  
+  
+  io.on("connection", (socket) => {
+    console.log(`ID: ${socket.id}`);
+  });
+  
+  server.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${ process.env.PORT }`);
+  });
